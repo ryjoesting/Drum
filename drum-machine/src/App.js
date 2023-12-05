1b131519c87fa9787b3a74b2846454c54464e229
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-12-05 10:55:39 -0600

    Init commit

---
+++ drum-machine/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Card from './Card'
+
+function App() {
+  return (
+    <div className="App d-flex justify-content-center vh-100 align-items-center">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
