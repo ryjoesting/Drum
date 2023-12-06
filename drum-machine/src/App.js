8addb1ae2b186c7cbbc50bf66016cc73d9e67d04
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-12-06 09:30:54 -0600

    Reworked CSS grid, added sound, and keyboard input

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,9 +1,77 @@
 import './App.css';
-import Card from './Card'
+import Drum from './Drum';
+import { useEffect } from 'react'
+
+const audioClips = [
+  {
+    keyTrigger: "Q",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
+    description: "Heater 1",
+  },
+  {
+    keyTrigger: "W",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+    description: "Heater 2",
+  },
+  {
+    keyTrigger: "E",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
+    description: "Heater 3",
+  },
+  {
+    keyTrigger: "A",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
+    description: "Heater 4",
+  },
+  {
+    keyTrigger: "S",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
+    description: "Clap",
+  },
+  {
+    keyTrigger: "D",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
+    description: "Open HH",
+  },
+  {
+    keyTrigger: "Z",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
+    description: "Kick n' Hat",
+  },
+  {
+    keyTrigger: "X",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
+    description: "Kick",
+  },
+  {
+    keyTrigger: "C",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    description: "Closed HH",
+  },
+];
 
 function App() {
+
+  const playSoundByKey = (e) => {
+    const soundClipObj = audioClips.find( (clip) => clip.keyTrigger === e.key.toUpperCase());
+    if (!soundClipObj) {console.log('soundClipObj not found'); return;}
+    document.getElementById(soundClipObj.keyTrigger).play().catch(console.error);
+    document.getElementById('display').innerText = soundClipObj.description;
+    document.getElementById('drum-' + soundClipObj.keyTrigger).focus();
+  }
+
+  useEffect( () => {
+    document.addEventListener('keydown', playSoundByKey)
+  } ,[]);
+
   return (
-    <Card />
+    <div className='container' id='drum-machine' onKeyDown={playSoundByKey}>
+      <h1>Play the drum machine!</h1>
+      <div className='drum-grid'>
+        {audioClips.map( (clipObj) => (<Drum audioClipObj={clipObj} key={clipObj.keyTrigger}/>))}
+      </div>
+      <h6 id='display'></h6>
+    </div>
   );
 }
 
--- drum-machine/src/Drum.js
+++ drum-machine/src/Drum.js
@@ -1,8 +1,25 @@
 import {useState} from 'react'
+ // uses ES6 destructuring to take audioClip object as props
+function Drum( { audioClipObj } ) {
+    //plays the sound if button is directly clicked
+    const playSound = (clip) => {
+        document.getElementById(clip.keyTrigger).play().catch(console.error); //finds the HTML audio clip with ID of whatever key was pressed
+        document.getElementById('drum-' + clip.keyTrigger).focus() //sets focus on the drum pad
+        document.getElementById('display').innerText = clip.description;
+    }
 
-function DrumPadGrid() {
     return (
-    <div className='container drum-pad-grid-container'>
+    <button className='drum-pad' id={'drum-' + audioClipObj.keyTrigger} text={audioClipObj.keyTrigger} onClick={() => playSound(audioClipObj)}>
+        <audio id={audioClipObj.keyTrigger} src={audioClipObj.url}/>
+        {audioClipObj.keyTrigger}
+    </button>
+    )
+}
+
+export default Drum;
+
+/*
+<div className='container drum-pad-grid-container'>
         <div className='row btn-row'>
             <div className='col-4'>
                 <button className='btn btn-primary btn-block'>#</button>
@@ -37,7 +54,4 @@ function DrumPadGrid() {
             </div>
         </div>
     </div>
-    )
-}
-
-export default DrumPadGrid;
+*/
